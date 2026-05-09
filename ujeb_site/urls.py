43b39411fde5e4897dee5434ba5a3b1a65c0c77a
You <youemery@gmail.com>
2026-05-09 11:28:28 +0100

    mise à jour du site

--- ujeb_site/urls.py
+++ ujeb_site/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path
-from setuptools.extern import names
 from django.conf import settings
 from django.conf.urls.static import static
 from core.views import home, members_page, partners_page
